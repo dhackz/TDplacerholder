5148c0cf11ab11a4573d3ace860bdc6bd2721a87
Harald <bharald@student.chalmers.se>
2021-06-09 23:13:00 +0200

    Implement NinjaTower

--- src/asset_manager.rs
+++ src/asset_manager.rs
@@ -7,6 +7,7 @@ use ggez::{
 pub struct AssetManager {
     pub tower_sprite: graphics::Image,
     pub tower_attack_sound: audio::Source,
+    pub ninja_tower_strong_attack_sound: audio::Source,
     pub monster_sprite: graphics::Image,
     pub gold_sprite: graphics::Image,
     pub base_sprite: graphics::Image,
@@ -18,6 +19,7 @@ impl AssetManager {
         AssetManager {
             tower_sprite: graphics::Image::new(ctx, "/tower2.png").unwrap(),
             tower_attack_sound: audio::Source::new(ctx, "/tower_attack.ogg").unwrap(),
+            ninja_tower_strong_attack_sound: audio::Source::new(ctx, "/NinjaTowerStrongAttack.mp3").unwrap(),
             monster_sprite: graphics::Image::new(ctx, "/monster1.png").unwrap(),
             gold_sprite: graphics::Image::new(ctx, "/gold_pile.png").unwrap(),
             base_sprite: graphics::Image::new(ctx, "/base.png").unwrap(),
